98bc98a0e7f19e6cd40206a9429c7f941c5c0e00
Cecília 💋 <ceciliavieiradossantosvieira0@gmail.com>
2026-05-11 15:55:28 -0300

    Add files via upload

--- Projeto final-Cecília/feijoada/js/dados.js
+++ Projeto final-Cecília/feijoada/js/dados.js
@@ -3,6 +3,10 @@
 const ITENS = [
   { nome: "Feijoada Individual", descricao: "A feijoada invidual tem como ingredientes linguiça, carne seca e costela.", preco: "R$ 15,00" },
   { nome: "Feijoada Família", descricao: "A feijoada família pode alimentar até 4 pessoas.É uma feijoada que tem como ingredientes várias carnes.", preco: "R$ 50,00" },
+  { nome: "Arroz", descricao: "arroz branco.", preco: "R$ 7,00"},
+  { nome: "Torresmo", descricao: "barrida de porco crocante.", preco: "R$ 15,00"},
+  { nome: "Farofa", descricao: "Uma deliciosa farofa com calabresa e bacon.", preco: "R$ 7,00"},
+  { nome: "Bisteca", descricao: "carne de porco temperada.", preco: "R$ 8,00"},
+  { nome:  "Couve Refogada", descricao: "passada no azeite e alho.", preco: "R$ 10,00" },
   { nome: "Caipirinha", descricao: "Uma bebida alcólica com cachaça e limão.", preco: "R$ 10,00" },
-  { nome: "Couve Refogada", descricao: "Descrição breve do item.", preco: "R$ 25,00" }
 ];
